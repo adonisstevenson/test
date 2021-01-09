2fa2ac86dbd55b39b4588955325c41d8d98a5647
adonisstevenson <adonisstevenson444@gmail.com>
2021-01-09 20:36:55 +0100

    danielek

--- danielo.php
+++ danielo.php
@@ -1,6 +1,34 @@
 <?php
 
-print("huj");
+$first = "jebac";
+$second = "legie";
 
+//echo $first." ".$second;
+
+$tab = [6, 5, 4, 3, 2, 1, 0];
+
+
+for($i = 0; $i<=count($tab)-1; $i++){
+	//echo $tab[$i];
+}
+//echo '<br>';
+//echo 'count: '.count($tab);
+
+
+$capital = 67;
+
+print("Zmienne capital ma wartość $capital");
+print("<h1>$capital</h1>");
+
+$first = 1.0000;
+$second = 2;
+$third = "napis";
+$fifth = $first + $second;
+
+$test = 1;
+
+$test = strval($test);
+
+echo gettype($test);
 
 ?>
